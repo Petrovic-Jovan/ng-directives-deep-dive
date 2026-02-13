06a896673b2ecc70370f5b4895367ea920d23f75
jovan.petrovic <jovanloki@gmail.com>
2026-02-13 13:33:22 +0100

    Enhanced custom directive with input parameter. Tempered with output hreef value in my fist custom directive

--- src/app/safe-link.directive.ts
+++ src/app/safe-link.directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from '@angular/core';
+import { Directive, input } from '@angular/core';
 
 @Directive({
   selector: 'a[appSafeLink]',
@@ -8,10 +8,17 @@ import { Directive } from '@angular/core';
   },
 })
 export class SafeLinkDirective {
+  customUrlExt = input<string>('myApp', { alias: 'appSafeLink' });
+
   onConfirmLeavePage(event: MouseEvent): void {
     const leave = window.confirm('Are you sure you want to leave this page?');
 
     if (leave) {
+      const target = (event.target as HTMLAnchorElement).href;
+      console.log(`Adress shown to user: ${target}...`);
+      (event.target as HTMLAnchorElement).href =
+        target + '?from=' + this.customUrlExt();
+      console.log(`Actual URL: ${(event.target as HTMLAnchorElement).href}...`);
       return;
     }
 
